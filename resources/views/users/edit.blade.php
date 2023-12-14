49637d8380528a5248f98906abafe2f05d3e6c36
dimabkst <dimabkst@gmail.com>
2023-12-14 22:42:13 +0200

    edit view update

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -15,29 +15,40 @@
         </h2>
     </x-slot>
 
-    <button>
-        <a href="{{ route('users.show', $user) }}">User Details</a>
-    </button>
+    <div class="py-12 px-12">
+        <div class="max-w-7xl m-auto sm:px-6 lg:px-8 flex items-center justify-center">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg w-2/5  min-h-[250px]">
+                <div class="p-2">
+                    <button class="default-button w-2/7">
+                        <a href="{{ route('users.show', $user) }}">&larr; User Details</a>
+                    </button>
+                </div>
 
-    <form method="POST" action="{{ route('users.update', $user) }}">
-        @csrf
-        @method('PUT')
+                <form method="POST" action="{{ route('users.update', $user) }}">
+                    @csrf
+                    @method('PUT')
+                    <div class="py-4 px-16 flex flex-col justify-items-center">
+                        <div class="p-2 form-group">
+                            <label for="name">Name</label>
+                            <input class="form-control" type="text" name="name" id="name" value="{{ old('name') ?? $user->name }}">
+                        </div>
 
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" name="name" id="name" class="form-control" value="{{ old('name') ?? $user->name }}">
+                        <div class="p-2 form-group">
+                            <label for="email">Email</label>
+                            <input class="form-control " type="email" name="email" id="email" value="{{ old('email') ?? $user->email }}">
+                        </div>
+
+                        <div class="p-2 form-group">
+                            <label for="password">Password</label>
+                            <input class="form-control" type="password" name="password" id="password">
+                        </div>
+                    </div>
+
+                    <div class="pb-6 pt-2 flex justify-center">
+                        <button class="edit-button" type="submit">Update User</button>
+                    </div>
+                </form>
+            </div>
         </div>
-
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" class="form-control" value="{{ old('email') ?? $user->email }}">
-        </div>
-
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" class="form-control">
-        </div>
-
-        <button type="submit" class="btn btn-primary">Update User</button>
-    </form>
+    </div>
 </x-app-layout>
